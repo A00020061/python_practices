87ca14b91c39c857ccf8eb0cf54060dc1ea2e74b
sandeep kumar <sandeepkumar4405@gmail.com>
2021-09-30 22:54:23 +0930

    add new code about append,remove,pop

--- doublelinkedlist.py
+++ doublelinkedlist.py
@@ -17,3 +17,156 @@ class Doublelinkedlist:
         while temp is not None:
             print(temp.value)
             temp=temp.next
+    
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 01:26:27 2021
+
+@author: sande
+"""
+
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+        self.prev=None
+        
+class Doublelinkedlist:
+    def __init__(self,value):
+        node=Node(value)
+        self.head=node
+        self.tail=node
+        self.length=1
+        
+    def print_list(self):
+        temp=self.head
+        while temp is not None:
+            print(temp.value)
+            temp=temp.next
+            
+    def append(self,value):
+        node=Node(value)
+        if self.head is None:
+            self.head=node
+            self.tail=node
+            
+        else:
+            self.tail.next=node
+            node.prev=self.tail
+            self.tail=node
+            
+        self.length +=1
+        return True
+    
+    def pop(self):
+        if self.length ==0:
+            return None
+        temp=self.tail
+        if self.length==1:
+            self.head=None
+            self.tail=None
+        else:
+            self.tail=self.tail.prev
+            self.tail.next=None
+            temp.prev=None
+        self.length-=1
+        return temp.value
+    
+    def prepend(self,value):
+        node=Node(value)
+        if self.length==0:
+            self.head=node
+            self.tail=node
+            self.prev=None
+        else:
+            
+            node.next=self.head
+            self.head.prev=node
+            self.head=node
+            
+        self.length+=1
+        return True
+    
+    def pop_first(self):
+        if self.length==0:
+            return None
+        temp=self.head
+        if self.length==1:
+            self.head=None
+            self.tail=None
+        
+        else:
+            self.head=self.head.next
+            self.head.prev=None
+            temp.next=None
+        self.length-=1
+        return temp.value
+    
+    
+    def get(self,index):
+        if index<0 or index>self.length:
+            return None
+        temp=self.head
+        if index<self.length/2:
+            for _ in range(index):
+                temp=temp.next
+        else:
+            temp=self.tail
+            for _ in range(self.length-1, index, -1):
+                temp=temp.prev
+            return temp
+        
+    def set(self,index,value):
+        temp=self.get(index)
+        if temp:
+            temp.value=value
+            return True
+        return False
+    
+    
+    def inert(self,index,value):
+         if index<0 or index>self.length:
+             return False
+         if index==0:
+             return self.prepend(value)
+         if index==self.length:
+             return self.append(value)
+         node=Node(value)
+         before=self.get(index-1)
+         after=before.next
+         
+         node.prev=before
+         node.next=after
+         before.next=node
+         after.prev=node
+         self.length+=1
+         return True
+     
+     def remove(self,index):
+         
+         if index<0 or index>self.length:
+             return None
+         if index==0:
+             return self.pop_first()
+         if index==self.length:
+             return self.pop()
+         temp=self.get(index)
+         temp.next.prev=temp.prev
+         temp.prev.next=temp.next
+         temp.next=None
+         temp.prev=None
+         self.length-=1
+         return temp
+         
+         
+         
+          
+         
+            
+        
+            
+        
+
+            
+        
+            

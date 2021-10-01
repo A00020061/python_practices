90b5c67d15f7393ad04d72ab62a90a729f30440e
sandeep kumar <sandeepkumar4405@gmail.com>
2021-10-01 17:09:16 +0930

    update color file and linkedlistpart1 file

--- color.py
+++ color.py
@@ -1,20 +1,2 @@
 
 
-Whenever I sit to code my next new project or just code something for the fun of it, I never really think about what I would need to consider should I decide to refactor it later on. Then during one of my classes,
- I learnt about design patterns and they sounded interesting, even though I didn’t understand them because the language was too technical lol.
-
-This blog post is the first in a series of design patterns and will be me sharing what I know about design patterns and how they can be used during your software development and maintenance. 
-This is meant to help me and also someone out there learn what design patterns are.
-
-Now that we have gotten past that very awkward introduction lol, let us actually look at what design patterns are in the simplest way.
-
-dp.jpg
-whenever i sit to code my next new project or just code something for the fun of it, i never really think about what i would need to consider sould i decide to refactor it later on. then during of my classes, 
-i learnt about design patterns and they sounded interesting, even though i didn't understand them because the language was to techiinal lol.
-
-this blog post is the first in series of desigh a-patterns adn will be me shareing what i knnw about desing patterns and how they can be used during you softare developeent and maintenance.
-
-in this line i want to explain or want to get pull request's conflict and hoope i will try to solve it on time. this is the new step of pull request to learn in better way
-
-now i am going to change sometiing in this file and want to get pull's conflict. hope so i will get it with this liine.
-

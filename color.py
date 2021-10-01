93228a429e080232625242fb35fa34d5e53a2ff8
sandeep kumar <sandeepkumar4405@gmail.com>
2021-10-01 17:04:06 +0930

    update color file

--- color.py
+++ color.py
@@ -1,16 +1,70 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+    
+class Linkedlist:
+    def __init__(self):
+        self.head=None
+        self.length=0
+
+    def print_list(self):
+        temp=self.head
+        while temp is not None:
+            print(temp.value,end=' ')
+            temp=temp.next
+
+    def prepend(self,value):
+        node=Node(value)
+        if self.length==0:
+            self.head=node
+            self.tail=node
+        else:
+            node.next=self.head
+            self.head=node
+        self.length +=1
+        return True
+    
+    def remove(self,index):
+        if index<0 or index>self.length:
+            return None
+        temp=self.head
+        
+        if index==0:
+            self.head=self.head.next
+            temp.next=None
+        if index==self.length-1:
+            while temp.next:
+                pre=temp
+                temp=temp.next
+            self.tail=pre
+            self.tail.next=None
+        for _ in range(index):
+            pre=temp
+            temp=temp.next
+        temp=pre.next
+        pre.next=temp.next
+        temp.next=None
+        self.length-=1
+        return temp        
 
 
-Whenever I sit to code my next new project or just code something for the fun of it, I never really think about what I would need to consider should I decide to refactor it later on. Then during one of my classes,
- I learnt about design patterns and they sounded interesting, even though I didn’t understand them because the language was too technical lol.
 
-This blog post is the first in a series of design patterns and will be me sharing what I know about design patterns and how they can be used during your software development and maintenance. 
-This is meant to help me and also someone out there learn what design patterns are.
 
-Now that we have gotten past that very awkward introduction lol, let us actually look at what design patterns are in the simplest way.
 
-dp.jpg
-whenever i sit to code my next new project or just code something for the fun of it, i never really think about what i would need to consider sould i decide to refactor it later on. then during of my classes, 
-i learnt about design patterns and they sounded interesting, even though i didn't understand them because the language was to techiinal lol.
+        
+            
 
-this blog post is the first in series of desigh a-patterns adn will be me shareing what i knnw about desing patterns and how they can be used during you softare developeent and maintenance.
-in this line i want to explain or want to get pull request's conflict and hoope i will try to solve it on time. this is the new step of pull request to learn in better way
+
+
+
+my=Linkedlist()
+my.prepend(10)
+my.prepend(20)
+my.prepend(30)
+my.prepend(40)
+my.remove(0)
+
+my.print_list()
+
+        
